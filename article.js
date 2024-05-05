afba1adfb097c09c32978c80c33ab3b7550f66aa
Ifarra <ifarra1324@gmail.com>
2024-05-06 00:02:07 +0700

    Responsive Home

--- article.js
+++ article.js
@@ -72,9 +72,9 @@ function sendTalent(res) {
         </button>
         <div class="collapse navbar-collapse justify-content-center NewArrival__header--title" id="main_nav">
             <ul class="navbar-nav ms-1 me-1 pacifico " style="gap: 15px;">
-                <li class="nav-item"><a class="nav-link text-center" href="/article"> - Article - </a></li>
+                <li class="nav-item"><a class="nav-link text-center" href="#"> - Article - </a></li>
                 <li class="nav-item"><a class="nav-link text-center" href="/"> - Home - </a></li>
-                <li class="nav-item"><a class="nav-link text-center" href="#"> - Talent - </a></li>
+                <li class="nav-item"><a class="nav-link text-center" href="/talent"> - Talent - </a></li>
             </ul>
         </div>
     </div>
@@ -150,24 +150,61 @@ function sendTalentdesc(res, numtalent) {
             <html>
                 <head>
                     <title>Holopage-Talent : ${numtalent}</title>
+                    <link rel="stylesheet" href="/public/css/talent.css">
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
                     <meta charset="UTF-8">
                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 </head>
                 <body>
-                    <main>
-                        <div class="blog-entry d-flex flex-column flex-md-row blog-entry-search-item">
-                          <a href="single.html" class="img-link me-4">
-                            <div style="width: 500px;">
+
+<nav class="navbar fixed-top navbar-expand-lg" style="background-color: #fff; z-index: 1112;">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-center NewArrival__header--title" id="main_nav">
+            <ul class="navbar-nav ms-1 me-1 pacifico " style="gap: 15px;">
+                <li class="nav-item"><a class="nav-link text-center" href="#"> - Article - </a></li>
+                <li class="nav-item"><a class="nav-link text-center" href="/"> - Home - </a></li>
+                <li class="nav-item"><a class="nav-link text-center" href="/talent"> - Talent - </a></li>
+            </ul>
+        </div>
+    </div>
+    <style>
+        .navbar-nav li a {
+            text-decoration: none;
+        }
+
+        .navbar-nav li a:hover {
+            text-decoration: underline;
+        }
+    </style>
+</nav>
+
+                    <main style="padding-top: 80px;">
+                        <div class="blog-entry d-flex flex-column blog-entry-search-item justify-content-center align-items-center">
+                        <div class="col-md-9">
+                          <a href="" class="d-flex justify-content-center">
+                            <div class="img-fluid">
                               <img src="${imgUrl}" alt="Article Image" style="width: 100%; height: auto;">
                             </div>
                           </a>
-                          <div>
+                          <div class="col-12 p-4">
                             <span class="date">Apr. ${suffix}, 2024</span>
-                            <h2><a href="/article/${folder}">${title}</a></h2>
-                            <p>${content}</p>
+                            <h1><a href="/article/${folder}">${title}</a></h1>
+                            <p class="text-break">${content}</p>
                             <p><a href="/article" class="btn btn-sm btn-outline-primary">Back</a></p>
                           </div>
+                          </div>
                         </div>
+
+                         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+		crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+		integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
+		crossorigin="anonymous" referrerpolicy="no-referrer"></script>
                     </main>
                 </body>
             `;
--- talent.js
+++ talent.js
@@ -136,19 +136,73 @@ function sendTalentdesc(res, numtalent) {
       }
 
       data = data.replace(/\n/g, "<br/>");
+      const img = `<img src="/public/HolopageTalent/${targetFile.replace(
+        /\.txt/,
+        ".png"
+      )}" style="width: 100%; height: auto;">`;
+
       const html = `
             <!DOCTYPE html>
             <html>
                 <head>
                     <title>Holopage-Talent : ${numtalent}</title>
                     <link rel="stylesheet" href="/public/css/talent.css">
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
                     <meta charset="UTF-8">
                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 </head>
                 <body>
+
+<nav class="navbar fixed-top navbar-expand-lg" style="background-color: #fff; z-index: 1112;">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-center NewArrival__header--title" id="main_nav">
+            <ul class="navbar-nav ms-1 me-1 pacifico " style="gap: 15px;">
+                <li class="nav-item"><a class="nav-link text-center" href="#"> - Article - </a></li>
+                <li class="nav-item"><a class="nav-link text-center" href="/"> - Home - </a></li>
+                <li class="nav-item"><a class="nav-link text-center" href="/talent"> - Talent - </a></li>
+            </ul>
+        </div>
+    </div>
+    <style>
+        .navbar-nav li a {
+            text-decoration: none;
+        }
+
+        .navbar-nav li a:hover {
+            text-decoration: underline;
+        }
+    </style>
+</nav>
+
                     <main>
-                        ${data}
+                                           <main style="padding-top: 80px;">
+                        <div class="blog-entry d-flex flex-column blog-entry-search-item justify-content-center align-items-center">
+                        <div class="col-md-9">
+                          <a href="" class="d-flex justify-content-center">
+                            <div class="img-fluid">
+                              ${img}
+                            </div>
+                          </a>
+                          <div class="col-12 p-4">
+                            <h1>${numtalent}</h1>
+                            <p class="text-break">${data}</p>
+                            <p><a href="/article" class="btn btn-sm btn-outline-primary">Back</a></p>
+                          </div>
+                          </div>
+                        </div>
                     </main>
+
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+		crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+		integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
+		crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
                 </body>
             `;
 
